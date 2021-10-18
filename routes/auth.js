440300f08e584c9cdeb03ad70d89977a6b8bb05e
desxz <muratgun545@gmail.com>
2021-10-18 15:29:39 +0300

    Authentication Controller-Route-MW

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+    register,
+    login,
+    getMe,
+    forgotPassword,
+    resetPassword,
+    updateDetails,
+    updatePassword,
+    logout,
+} = require("../controllers/auth");
+const { protect } = require("../middleware/auth");
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resettoken", resetPassword);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
+router.get("/logout", logout);
+
+module.exports = router;
