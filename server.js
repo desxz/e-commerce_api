7c15cca605af542a64c6b9fa7a16c7f01eb9efd2
desxz <muratgun545@gmail.com>
2021-10-31 01:54:28 +0300

    Docgen API Documentation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const colors = require("colors");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const path = require("path");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -21,6 +22,9 @@ const PORT = process.env.PORT || 5000;
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // API Route shortway
 const api = process.env.API_URL;
